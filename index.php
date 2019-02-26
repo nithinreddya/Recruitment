4a05ca1c969b1cc9ec499a496a393ae13a5433dc
Shiva Prasad <svsshivaprasad@gmail.com>
2019-02-26 14:02:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,22 +10,9 @@
 }
 </style>
 <script src="//cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js"></script>
-  <script>
-	if (annyang) {
-	  // Let's define our first command. First the text we expect, and then the function it should call
-	  var commands = {
-		'hello': function() {
-		  location.assign("http://localhost/RP");
-		}
-	  };
-	  // Add our commands to annyang
-	  annyang.addCommands(commands);
-	  // Start listening. You can call this here, or attach this call to an event, button, etc.
-	  annyang.start();
-	}
-</script>
-<body id = "body" class ="bg">
 
+<body id = "body" class ="bg">
+<img src = "images/icons/logo1.png" style="position:relative;top:200px;">
 	<div class="dropdown" id = "btn">
 		<button onclick="myFunction()" class="dropbtn">Login as</button>
 		  <div id="myDropdown" class="dropdown-content">
@@ -55,3 +42,4 @@
 	</script>
 </body>
 </html>
+
